6c674495acb21d357d1e3831994f363633313c5e
Luis Antonio Mendoza <luismendoza.dev@gmail.com>
2019-07-24 09:27:49 -0500

    Reading and storing data/messages to Firebase

---
+++ firebase.config.example.js
@@ -0,0 +1,10 @@
+// Firebase configuration
+var firebaseConfig = {
+  apiKey: "apiKey",
+  authDomain: "example.firebaseapp.com",
+  databaseURL: "https://example.firebaseio.com",
+  projectId: "example",
+  storageBucket: "",
+  messagingSenderId: "messagingSenderId",
+  appId: "appId"
+}
